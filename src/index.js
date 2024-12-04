d77d859cf2c2852882e84ef17204f95826ef612e
Israel Macedo da Silva <raelmacedodasilva@gmail.com>
2024-12-04 14:50:54 -0300

    feat: implementar função cadastrar para salvar usuário no localStorage

--- src/index.js
+++ src/index.js
@@ -6,3 +6,19 @@ function criarUsuario(nome, telefone, email, nascimento) {
         nascimento: nascimento
     };
 }
+
+function cadastrar() {
+    
+    let nome = document.getElementById('id_nome').value;
+    let telefone = document.getElementById('id_telefone').value;
+    let email = document.getElementById('id_email').value;
+    let nascimento = document.getElementById('id_nascimento').value;
+    let usuario = criarUsuario(nome, telefone, email, nascimento);
+    let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+    usuarios.push(usuario);
+
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+    alert('Usuário cadastrado com sucesso!');
+
+}
