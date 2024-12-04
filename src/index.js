3f490dff3530bbf1baad22e7d22dcbe79a500a1c
Israel Macedo da Silva <raelmacedodasilva@gmail.com>
2024-12-04 14:49:47 -0300

    feat: adicionar função criarUsuario ao index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function criarUsuario(nome, telefone, email, nascimento) {
+    return {
+        nome: nome,
+        telefone: telefone,
+        email: email,
+        nascimento: nascimento
+    };
+}
